13068b68f5d90ede9557bf2051ccdbb286b5af54
Victor Momodu <victoramomodu@gmail.com>
2026-03-24 14:31:39 +0100

    feat: remove styles.css

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=FileResponse)
